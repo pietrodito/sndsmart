e492c922c67fe94e4694fa2e4718984ffec42de9
pietrodito <pietrodito@gmail.com>
2024-06-17 11:35:11 +0200

    Add .onLoad message

--- R/aaa.R
+++ R/aaa.R
@@ -1,3 +1,12 @@
 the <- new.env(parent = emptyenv())
 
+.onAttach <-function(libname, pkgname) {
+
+  version_file <- system.file("extdata", "version", package = "sndsmart")
+  version <- read.delim(version_file, colClasses = c("character"), header = F)
+
+  packageStartupMessage("Bienvenue dans le package sndsmart !")
+  packageStartupMessage(glue::glue("Vous uilisez la version {version}"))
+}
+
 
